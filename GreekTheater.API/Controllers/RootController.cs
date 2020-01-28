dbcd2523802b339c414640268dd90e64e6de8961
rakinas <aris.triantafyllou@outlook.com>
2020-01-28 22:01:42 +0200

    Add Root Controller

---
+++ GreekTheater.API/Controllers/RootController.cs
@@ -0,0 +1,27 @@
+﻿using GreekTheater.API.Core.Services.Pagination;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GreekTheater.API.Controllers
+{
+    [Route("api")]
+    [ApiController]
+    public class RootController : ControllerBase
+    {
+        private readonly IRootPaginationService _linksService;
+
+        public RootController(IRootPaginationService linksService)
+        {
+            _linksService = linksService ?? throw new ArgumentNullException(nameof(linksService));
+        }
+
+        [HttpGet(Name = "GetRoot")]
+        public IActionResult GetRoot()
+        {
+            return Ok(_linksService.CreateRootLinks());
+        }
+    }
+}
